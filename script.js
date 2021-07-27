a731083837fa42714e858753ec75a764dd41be65
Jolita Sapaliene <jolita.sapaliene@gmail.com>
2021-07-27 20:06:14 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+/* ------------------------------ TASK 2 ----------------------------
+Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
+su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
+pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
+------------------------------------------------------------------- */
+let count = 0;
+const stateElement = document.getElementById("btn__state");
+
+document.getElementById("btn__element").addEventListener("click", countClick);
+
+function countClick(e) {
+  e.preventDefault();
+  stateElement.textContent = ++count;
+}
